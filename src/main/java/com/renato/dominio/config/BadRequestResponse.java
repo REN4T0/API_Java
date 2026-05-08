d35f63dd1f65dd22e7aa1a30c61074ed432ee219
renato <renatoviturino80@gmail.com>
2026-05-08 13:29:21 -0300

    Ajustando logs

--- src/main/java/com/renato/dominio/config/BadRequestResponse.java
+++ src/main/java/com/renato/dominio/config/BadRequestResponse.java
@@ -14,7 +14,7 @@ public class BadRequestResponse {
 
     @ServerExceptionMapper
     public Response getMismatchedInputError(MismatchedInputException exMismatchedInput) {
-        Log.warnf("| O TIPO DO(S) VALOR(ES) É INVALIDO. OS ATRIBUTOS DEVEM SER EXCLUSIVAMENTE DO TIPO STRING.\nCÓDIGO: 400 - BAD REQUEST\nDETALHES: %s", exMismatchedInput);
+        Log.warnf("| O TIPO DO(S) VALOR(ES) É INVALIDO. OS ATRIBUTOS DEVEM SER EXCLUSIVAMENTE DO TIPO STRING.\nCÓDIGO: 400 - BAD REQUEST\nDETALHES: %s\n", exMismatchedInput);
 
         // Erro personalizado e incluindo detalhes nativos do erro
         Status errorAnswer = new Status("400", "bad_request", "Os atributos do objeto enviado deve conter exclusivamente dados do tipo String");
@@ -26,7 +26,7 @@ public class BadRequestResponse {
     @ServerExceptionMapper
     public Response getConstraintViolationError(ConstraintViolationException exConstraintViolation) {
         String errorMsg = exConstraintViolation.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(" | "));
-        Log.warnf("| A REQUISIÇÃO FOI REJEITADA PELOS DECORATORS. ALGUM DADO INVÁLIDO FOI ENVIADO.\nCÓDIGO: 400 - BAD REQUEST\nDETALHES: %s", errorMsg);
+        Log.warnf("| A REQUISIÇÃO FOI REJEITADA PELOS DECORATORS. ALGUM DADO INVÁLIDO FOI ENVIADO.\nCÓDIGO: 400 - BAD REQUEST\nDETALHES: %s\n", errorMsg);
         Status errorAnswer = new Status("400", "bad_request", errorMsg);
 
         return Response.status(Response.Status.BAD_REQUEST).entity(errorAnswer).build();
--- src/main/java/com/renato/dominio/controller/ReceiveDogData.java
+++ src/main/java/com/renato/dominio/controller/ReceiveDogData.java
@@ -39,7 +39,7 @@ public class ReceiveDogData {
 
     @POST
     public Response receiveToPost(@Valid DogsDTO DogDtoObj) {
-        Log.infof("| REQUISIÇÃO DE CADASTRO DE UM NOVO REGISTRO RECEBIDA. INICIANDO PROCEDIMENTO...\nDADOS RECEBIDOS -> Raça: %s, Apelido: %s, Sexo: %s\nIP QUE REQUISITOU A OPERAÇÃO: %s", DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender(), getClientIP());
+        Log.infof("| REQUISIÇÃO DE CADASTRO DE UM NOVO REGISTRO RECEBIDA. INICIANDO PROCEDIMENTO...\nDADOS RECEBIDOS -> Raça: %s, Apelido: %s, Sexo: %s\nIP QUE REQUISITOU A OPERAÇÃO: %s\n", DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender(), getClientIP());
         Status RESPONSE;
 
         try {
@@ -48,7 +48,7 @@ public class ReceiveDogData {
                 RESPONSE = new Status("200", "success", "O registro foi cadastrado com sucesso.");
                 RESPONSE.more_info = result;
 
-                Log.infof("| CADASTRO DE NOVO REGISTRO REALIZADO COM SUCESSO.\nDETALHES: %s", result);
+                Log.infof("| CADASTRO DE NOVO REGISTRO REALIZADO COM SUCESSO.\nDETALHES: %s\n", result);
                 return Response.ok(RESPONSE).build();
             }
 
@@ -57,18 +57,18 @@ public class ReceiveDogData {
         } catch (RecordAlreadyExistsException exRecord) {
             RESPONSE = new Status("409", "conflict", "O registro enviado já existe no banco de dados.");
             RESPONSE.more_info = exRecord;
-            Log.warnf("| NÃO FOI POSSÍVEL CADASTRAR O REGISTRO RECEBIDO, PORQUE ELE JÁ EXISTE NO BANCO DE DADOS.\nCÓDIGO: 409 - CONFLICT\nDADOS QUE NÃO FOI POSSÍVEL CADASTRAR -> Raça: %s, Apelido: %s, Sexo: %s", DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender());
+            Log.warnf("| NÃO FOI POSSÍVEL CADASTRAR O REGISTRO RECEBIDO, PORQUE ELE JÁ EXISTE NO BANCO DE DADOS.\nCÓDIGO: 409 - CONFLICT\nDADOS QUE NÃO FOI POSSÍVEL CADASTRAR -> Raça: %s, Apelido: %s, Sexo: %s\n", DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender());
             return Response.status(Response.Status.CONFLICT).entity(RESPONSE).build();
 
         } catch (BreedNotFoundException exBreedNotFound) {
             RESPONSE = new Status("404", "not_found", "A raça (breed) enviada é inválida/inexistente.");
-            Log.warnf("| NÃO FOI POSSÍVEL CADASTRAR O REGISTRO RECEBIDO, PORQUE A RAÇA ENVIADA É INVÁLIDA OU INEXISTENTE.\nCÓDIGO: 404 - NOT FOUND\nDADO COM CONTEÚDO INVÁLIDO/INEXISTENTE -> Raça: %s", DogDtoObj.getBreed());
+            Log.warnf("| NÃO FOI POSSÍVEL CADASTRAR O REGISTRO RECEBIDO, PORQUE A RAÇA ENVIADA É INVÁLIDA OU INEXISTENTE.\nCÓDIGO: 404 - NOT FOUND\nDADO COM CONTEÚDO INVÁLIDO/INEXISTENTE -> Raça: %s\n", DogDtoObj.getBreed());
             return Response.status(Response.Status.NOT_FOUND).entity(RESPONSE).build();
 
         } catch (Exception ex) {
             RESPONSE = new Status("500", "server_error", "Houve um problema na comunicação com o banco de dados no momento de cadastrar o registro.");
             RESPONSE.more_info = ex;
-            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE CADASTRAR UM NOVO REGISTRO.\nCÓDIGO: 500 - INTERNAL SERVER ERROR\nDETALHES - %s", ex);
+            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE CADASTRAR UM NOVO REGISTRO.\nCÓDIGO: 500 - INTERNAL SERVER ERROR\nDETALHES - %s\n", ex);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(RESPONSE).build();
         }
     }
@@ -76,7 +76,7 @@ public class ReceiveDogData {
     @PUT
     @Path("/{id}")
     public Response receiveToUpdate(@PathParam("id") UUID id, @Valid DogsDTO DogDtoObj) {
-        Log.infof("| REQUISIÇÃO DE ATUALIZAÇÃO DE REGISTRO RECEBIDA PARA O ID: %s \nDADOS RECEBIDOS -> Raça: %s, Apelido: %s, Sexo: %s\nIP QUE REQUISITOU A OPERAÇÃO: %s", id, DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender(), getClientIP());
+        Log.infof("| REQUISIÇÃO DE ATUALIZAÇÃO DE REGISTRO RECEBIDA PARA O ID: %s \nDADOS RECEBIDOS -> Raça: %s, Apelido: %s, Sexo: %s\nIP QUE REQUISITOU A OPERAÇÃO: %s\n", id, DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender(), getClientIP());
         Status RESPONSE;
 
         try {
@@ -85,7 +85,7 @@ public class ReceiveDogData {
                 RESPONSE = new Status("200", "success", "O registro foi atualizado com êxito.");
                 RESPONSE.more_info = result;
 
-                Log.infof("| O REGISTRO DE ID %s FOI ATUALIZADO COM SUCESSO.\nDETALHES: %s", id, result);
+                Log.infof("| O REGISTRO DE ID %s FOI ATUALIZADO COM SUCESSO.\nDETALHES: %s\n", id, result);
                 return Response.ok(RESPONSE).build();
             }
 
@@ -93,11 +93,11 @@ public class ReceiveDogData {
         } catch (BreedNotFoundException exBreedNotFound) {
             RESPONSE = new Status("404", "not_found", "A raça (breed) enviada é inválida/inexistente.");
             RESPONSE.more_info = exBreedNotFound;
-            Log.warnf("| O REGISTRO DE ID %s NÃO FOI ATUALIZADO, PORQUE A RAÇA ENVIADA É INVÁLIDA OU INEXISTENTE.\nCÓDIGO: 404 - NOT FOUND\nDADO COM CONTEÚDO INVÁLIDO/INEXISTENTE -> Raça: %s", id, DogDtoObj.getBreed());
+            Log.warnf("| O REGISTRO DE ID %s NÃO FOI ATUALIZADO, PORQUE A RAÇA ENVIADA É INVÁLIDA OU INEXISTENTE.\nCÓDIGO: 404 - NOT FOUND\nDADO COM CONTEÚDO INVÁLIDO/INEXISTENTE -> Raça: %s\n", id, DogDtoObj.getBreed());
             return Response.status(Response.Status.NOT_FOUND).entity(RESPONSE).build();
 
         } catch (RecordAlreadyExistsException exRecord) {
-            Log.warnf("| O REGISTRO DE ID %s NÃO FOI ATUALIZADO, PORQUE A COMBINAÇÃO DOS DADOS RECEBIDOS JÁ EXISTEM EM OUTRO REGISTRO NO BANCO DE DADOS.\nCÓDIGO: 409 - CONFLICT\nNÃO FOI POSSÍVEL ATUALIZAR OS SEGUINTES DADOS -> Raça: %s, Apelido: %s, Sexo: %s", id, DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender());
+            Log.warnf("| O REGISTRO DE ID %s NÃO FOI ATUALIZADO, PORQUE A COMBINAÇÃO DOS DADOS RECEBIDOS JÁ EXISTEM EM OUTRO REGISTRO NO BANCO DE DADOS.\nCÓDIGO: 409 - CONFLICT\nNÃO FOI POSSÍVEL ATUALIZAR OS SEGUINTES DADOS -> Raça: %s, Apelido: %s, Sexo: %s\n", id, DogDtoObj.getBreed(), DogDtoObj.getSurname(), DogDtoObj.getGender());
             RESPONSE = new Status("409", "conflict", "O registro já existe no banco de dados.");
             RESPONSE.more_info = exRecord;
             return Response.status(Response.Status.CONFLICT).entity(RESPONSE).build();
@@ -105,13 +105,13 @@ public class ReceiveDogData {
         } catch (RecordNotFoundException exNotFound) {
             RESPONSE = new Status("404", "not_found", "O Id informado nao foi encontrado na base de dados.");
             RESPONSE.more_info = exNotFound;
-            Log.warnf("| NÃO FOI POSSÍVEL ATUALIZAR, PORQUE O REGISTRO DE ID %s NÃO EXISTE NO BANCO DE DADOS.\nCÓDIGO: 404 - NOT FOUND", id);
+            Log.warnf("| NÃO FOI POSSÍVEL ATUALIZAR, PORQUE O REGISTRO DE ID %s NÃO EXISTE NO BANCO DE DADOS.\nCÓDIGO: 404 - NOT FOUND\n", id);
             return Response.status(Response.Status.NOT_FOUND).entity(RESPONSE).build();
 
         } catch (Exception ex) {
             RESPONSE = new Status("500", "server_error", "Houve um problema na comunicação com o banco de dados no momento de cadastrar o registro.");
             RESPONSE.more_info = ex;
-            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE ATUALIZAR O REGISTRO DE ID: %s\nCÓDIGO: 500 - INTERNAL SERVER ERROR\nDETALHES - %s", id, ex);
+            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE ATUALIZAR O REGISTRO DE ID: %s\nCÓDIGO: 500 - INTERNAL SERVER ERROR\nDETALHES - %s\n", id, ex);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(RESPONSE).build();
         }
     }
@@ -119,15 +119,15 @@ public class ReceiveDogData {
 
     @GET
     public Response receiveToGetAll() {
-        Log.infof("| REQUISIÇÃO DE CONSULTA DE REGISTROS CADASTRADOS NO BANCO DE DADOS RECEBIDA.\nIP QUE REQUISITOU A OPERAÇÃO: %s", getClientIP());
+        Log.infof("| REQUISIÇÃO DE CONSULTA DE REGISTROS CADASTRADOS NO BANCO DE DADOS RECEBIDA.\nIP QUE REQUISITOU A OPERAÇÃO: %s\n", getClientIP());
 
         try {
             final List<Dogs> RESPONSE = dogsService.getAllDogs();
-            Log.info("| CONSULTA DE REGISTROS CADASTRADOS CONCLUÍDA. RETORNANDO DADOS OBTIDOS PARA O CLIENTE...");
+            Log.info("| CONSULTA DE REGISTROS CADASTRADOS CONCLUÍDA. RETORNANDO DADOS OBTIDOS PARA O CLIENTE...\n");
             return Response.ok(RESPONSE).build();
 
         } catch (Exception ex) {
-            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE CONSULTAR OS REGISTROS CADASTRADOS.\nCÓDIGO: 500 - INTERNAL SERVER ERROR\n DETALHES - %s", ex);
+            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE CONSULTAR OS REGISTROS CADASTRADOS.\nCÓDIGO: 500 - INTERNAL SERVER ERROR\n DETALHES - %s\n", ex);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Status("500", "server_error", "Houve um problema na comunicação com o banco de dados no momento de buscar os registros.").more_info = ex).build();
         }
     }
@@ -135,7 +135,7 @@ public class ReceiveDogData {
     @GET
     @Path("/apelido/{surname}")
     public Response receiveToSearchSurname(@PathParam("surname") String surname) {
-        Log.infof("| REQUISIÇÃO DE BUSCA POR REGISTROS QUE CONTENHAM '%s' NO APELIDO RECEBIDA.\nIP QUE REQUISITOU A OPERAÇÃO: %s", surname, getClientIP());
+        Log.infof("| REQUISIÇÃO DE BUSCA POR REGISTROS QUE CONTENHAM '%s' NO APELIDO RECEBIDA.\nIP QUE REQUISITOU A OPERAÇÃO: %s\n", surname, getClientIP());
 
         try {
             final List<Dogs> RESPONSE = dogsService.searchDogsPerSurname(surname);
@@ -143,7 +143,7 @@ public class ReceiveDogData {
             return Response.ok(RESPONSE).build();
 
         } catch (Exception ex) {
-            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE BUSCAR UM REGISTRO PELO APELIDO '%s'.\nCÓDIGO: 500 - INTERNAL SERVER ERROR\n DETALHES - %s", surname, ex);
+            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DE BUSCAR UM REGISTRO PELO APELIDO '%s'.\nCÓDIGO: 500 - INTERNAL SERVER ERROR\n DETALHES - %s\n", surname, ex);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Status("500", "server_error", "Houve um problema na comunicação com o banco de dados no momento de buscar o registro com base no campo \"apelido\" (surname).").more_info = ex).build();
         }
     }
@@ -152,17 +152,17 @@ public class ReceiveDogData {
     @Path("/breeds")
     @Produces(MediaType.APPLICATION_JSON)
     public Response listAvailableBreeds() {
-        Log.infof("| BUSCANDO RAÇAS EXISTENTES/VÁLIDAS ATRAVÉS DE UMA REQUISIÇÃO EFETUADA NA API 'DOG CEO'...\nIP QUE REQUISITOU A OPERAÇÃO: %s", getClientIP());
+        Log.infof("| BUSCANDO RAÇAS EXISTENTES/VÁLIDAS ATRAVÉS DE UMA REQUISIÇÃO EFETUADA NA API 'DOG CEO'...\nIP QUE REQUISITOU A OPERAÇÃO: %s\n", getClientIP());
 
         try {
             final ArrayList<String> BREED_ARRAY_RESPONSE = validateBreed.makeBreedArray();
-            Log.info("| BUSCA POR RAÇAS VÁLIDAS DA API EXTERNA 'DOG CEO' CONCLUÍDA. RETORNANDO DADOS OBTIDOS PARA O CLIENTE...");
+            Log.info("| BUSCA POR RAÇAS VÁLIDAS DA API EXTERNA 'DOG CEO' CONCLUÍDA. RETORNANDO DADOS OBTIDOS PARA O CLIENTE...\n");
             return Response.ok(BREED_ARRAY_RESPONSE).build();
 
         } catch (Exception ex) {
             Status RESPONSE = new Status("503", "service_unavailable", "Houve um problema na comunicação com a API externa no momento de buscar a lista de raças existentes/válidas.");
             RESPONSE.more_info = ex;
-            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM A API 'DOG CEO' NO MOMENTO DE CONSULTAR RAÇAS VÁLIDAS PARA A APLICAÇÃO.\nCÓDIGO: 503 - SERVICE UNAVAILABLE\n DETALHES - %s", ex);
+            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM A API 'DOG CEO' NO MOMENTO DE CONSULTAR RAÇAS VÁLIDAS PARA A APLICAÇÃO.\nCÓDIGO: 503 - SERVICE UNAVAILABLE\n DETALHES - %s\n", ex);
             return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(RESPONSE).build();
         }
     }
@@ -177,13 +177,13 @@ public class ReceiveDogData {
     @DELETE
     @Path("/{id}")
     public Response receiveToDelete(@PathParam("id") UUID id) {
-        Log.infof("| REQUISIÇÃO DE EXCLUSÃO RECEBIDA PARA O REGISTRO DE ID: %s | INICIANDO PROCEDIMENTO...\nIP QUE REQUISITOU A OPERAÇÃO: %s", id, getClientIP());
+        Log.infof("| REQUISIÇÃO DE EXCLUSÃO RECEBIDA PARA O REGISTRO DE ID: %s | INICIANDO PROCEDIMENTO...\nIP QUE REQUISITOU A OPERAÇÃO: %s\n", id, getClientIP());
         Status RESPONSE;
 
         try {
             if (dogsService.deleteDogs(id)) {
                 RESPONSE = new Status("200", "success", "O registro foi deletado com sucesso.");
-                Log.infof("| REGISTRO DE ID %s DELETADO COM SUCESSO.", id);
+                Log.infof("| REGISTRO DE ID %s DELETADO COM SUCESSO.\n", id);
                 return Response.ok(RESPONSE).build();
             }
 
@@ -191,13 +191,13 @@ public class ReceiveDogData {
 
         } catch (RecordNotFoundException exRecordNotFound) {
             RESPONSE = new Status("404", "not_found", "O registro não foi deletado, porque ele não existe no banco de dados.");
-            Log.warnf("| NÃO FOI POSSÍVEL DELETAR, PORQUE O REGISTRO DE ID %s NÃO EXISTE NO BANCO DE DADOS.\nCÓDIGO: 404 - NOT FOUND", id);
+            Log.warnf("| NÃO FOI POSSÍVEL DELETAR, PORQUE O REGISTRO DE ID %s NÃO EXISTE NO BANCO DE DADOS.\nCÓDIGO: 404 - NOT FOUND\n", id);
             return Response.status(Response.Status.NOT_FOUND).entity(RESPONSE).build();
 
         } catch (Exception ex) {
             RESPONSE = new Status("500", "server_error", "Houve um problema na comunicação com o banco de dados no momento de deletar o registro.");
             RESPONSE.more_info = ex;
-            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DELETAR O REGISTRO DE ID: %s\nCÓDIGO: 500 - INTERNAL SERVER ERROR\n DETALHES - %s", id, ex);
+            Log.errorf("| OCORREU UM ERRO INESPERADO ENVOLVENDO A COMUNICAÇÃO COM O BANCO DE DADOS NO MOMENTO DELETAR O REGISTRO DE ID: %s\nCÓDIGO: 500 - INTERNAL SERVER ERROR\n DETALHES - %s\n", id, ex);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(RESPONSE).build();
         }
     }
